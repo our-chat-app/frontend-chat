8ea4fb100e91a1ffe57fafbbf31880d141048989
Patrik Arell <patrikarell@gmail.com>
2024-02-19 15:18:29 +0100

    validate on create user
    Co-authored-by: William Wibeck <WilliamWibeck@users.noreply.github.com>
    Co-authored-by: AndreasIshak1 <AndreasIshak1@users.noreply.github.com>
    Co-authored-by: Asrn <AsrinneNazari@users.noreply.github.com>

---
+++ login.js
@@ -0,0 +1,26 @@
+const loginButton = document.getElementById('login');
+const emailLogin = document.getElementById('email-login');
+const passwordLogin = document.getElementById('password-login');
+const error = document.getElementById('error');
+
+loginButton.addEventListener('click', async (ev) => {
+  ev.preventDefault();
+  const rawResponse = await fetch('http://localhost:3000/api/signIn', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({
+      email: emailLogin.value,
+      password: passwordLogin.value,
+    }),
+  });
+  if (rawResponse.status == 200) {
+    const content = await rawResponse.json();
+    window.location.replace('index.html');
+  } else {
+    error.style.display = 'block';
+  }
+});
---
+++ register.js
@@ -0,0 +1,29 @@
+const registerButton = document.getElementById('register');
+const passwordRegister = document.getElementById('password-register');
+const confirmPassword = document.getElementById('passwordconfirm-register');
+const nicknameRegister = document.getElementById('nickname-register');
+const emailRegister = document.getElementById('email-register');
+const error = document.getElementById('error');
+
+registerButton.addEventListener('click', async (ev) => {
+  ev.preventDefault();
+  const rawResponse = await fetch('http://localhost:3000/api/users', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({
+      email: emailRegister.value,
+      password: passwordRegister.value,
+      nickname: nicknameRegister.value,
+    }),
+  });
+  if (rawResponse.status == 201) {
+    const content = await rawResponse.json();
+    window.location.replace('index.html');
+  } else {
+    error.style.display = 'block';
+  }
+});
