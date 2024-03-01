0b309a4ce47de77c1525a0a66bac83283a24ac56
Patrik Arell <patrikarell@gmail.com>
2024-03-01 18:35:59 +0100

    message from client
    Co-authored-by: William Wibeck <WilliamWibeck@users.noreply.github.com>
    Co-authored-by: Asrn <AsrinneNazari@users.noreply.github.com>
    Co-authored-by: AndreasIshak1 <AndreasIshak1@users.noreply.github.com>

--- messages.js
+++ messages.js
@@ -1,22 +1,21 @@
-const submitButton = document.getElementById("message-submit-button");
-const messageInputField = document.getElementById("message-input");
+const submitButton = document.getElementById('message-submit-button');
+const messageInputField = document.getElementById('message-input');
 
-submitButton.addEventListener("submit", async (ev) => {
+submitButton.addEventListener('click', async (ev) => {
   ev.preventDefault();
-  const rawResponse = await fetch('http://localhost:3000/api/message',{
+  const rawResponse = await fetch('http://localhost:3000/api/message', {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
     method: 'POST',
     body: JSON.stringify({
-      messages: messageInputField.value,
-      //userId: .value,
+      message: messageInputField.value,
+      // userId:user.id
     }),
   });
   if (rawResponse.status == 201) {
     const content = await rawResponse.json();
     /* window.location.replace('index.html'); */
   }
-}
-);
+});
