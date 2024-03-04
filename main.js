3ae4017af5c1c960655f1e37d788154a8158e727
Patrik Arell <patrikarell@gmail.com>
2024-03-04 15:26:42 +0100

    write messages from frontend
    Co-authored-by: Asrn <AsrinneNazari@users.noreply.github.com>
    Co-authored-by: AndreasIshak1 <AndreasIshak1@users.noreply.github.com>
    Co-authored-by: William Wibeck <WilliamWibeck@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 // import './style.scss'
+const messageSender = document.getElementById('message-sender');
+let textBox = document.getElementById('chat-messages');
+async function getUsers() {
+  const rawResponse = await fetch('http://localhost:3000/api/users');
+  const allUsers = await rawResponse.json();
 
+  allUsers.forEach((user) => {
+    const senderLink = document.createElement('a');
+ 
+    
+  });
 
-/* Lägg in en forEach loop här nånstans och loopa alla meddelanden */
+  
+}
 
+export async function getMessages() {
+  textBox.innerHTML = '';
+  const rawResponse = await fetch('http://localhost:3000/api/messages');
+  const allMessages = await rawResponse.json();
+  //Kanske inte behöver hämta alla meddelanden?
+  allMessages.forEach((message) => {
+    if (message.isCurrentUser) {
+      const messageHolder = document.createElement('div');
+      const messageBox = document.createElement('div');
+      messageHolder.className = 'message-box-holder';
+      messageBox.className = 'message-box';
+      messageBox.innerHTML = message.message;
+      messageHolder.appendChild(messageBox);
+      textBox.appendChild(messageHolder);
+    } else {
+      const messageHolder = document.createElement('div');
+      const messageBox = document.createElement('div');
+      const messageSender = document.createElement('div');
+      
 
+      messageSender.classList.add('message-sender');
+      messageHolder.className = 'message-box-holder';
+      messageBox.classList.add('message-box', 'message-partner');
+
+      messageBox.innerHTML = message.message;
+      messageHolder.appendChild(messageSender);
+      messageHolder.appendChild(messageBox);
+      messageSender.appendChild(senderLink);
+      textBox.appendChild(messageHolder);
+    }
+  });
+}
+
+await getMessages();
--- messages.js
+++ messages.js
@@ -1,14 +1,19 @@
+import { getMessages } from './main.js';
+
 const submitButton = document.getElementById('message-submit-button');
 const messageInputField = document.getElementById('message-input');
+const messageSender = document.getElementById('message-sender');
 
 submitButton.addEventListener('click', async (ev) => {
   ev.preventDefault();
+
   const rawResponse = await fetch('http://localhost:3000/api/message', {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
     method: 'POST',
+    credentials: 'include',
     body: JSON.stringify({
       message: messageInputField.value,
       // userId:user.id
@@ -18,4 +23,7 @@ submitButton.addEventListener('click', async (ev) => {
     const content = await rawResponse.json();
     /* window.location.replace('index.html'); */
   }
+  await getMessages();
+  document.getElementById('message-input')
+  messageInputField.value = ''
 });
